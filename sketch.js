b96b346369663d24cc49d2468a872968dc8047e0
hamza-ladib <109552817+hamza-ladib@users.noreply.github.com>
2022-08-29 13:00:10 +0100

    commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+let canvas;
+let width=500;
+let height=570;
+function setup() {
+  
+   canvas= createCanvas(width,height);
+   canvas.id("cnv");
+
+  }
+  function draw() {
+    background("red");
+    
+  } 
+  
